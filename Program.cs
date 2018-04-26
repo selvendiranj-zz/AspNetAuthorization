36ca6bb1f9c29ebe0b388deffe3b3d15a5b63ce4
selvendiranj <selvendiranj@gmail.com>
2018-04-26 20:59:42 +0530

    Added Hsts, RequireHttpAttribute to all routes, certificate generator

--- Program.cs
+++ Program.cs
@@ -1,55 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using ContactManager.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace ContactManager
 {
     public class Program
     {
+        private static IConfiguration Config;
+
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
+            IWebHost host = BuildWebHost(args);
 
-            using (var scope = host.Services.CreateScope())
+            using (IServiceScope scope = host.Services.CreateScope())
             {
-                var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
-
+                IServiceProvider services = scope.ServiceProvider;
                 // requires using Microsoft.Extensions.Configuration;
-                var config = host.Services.GetRequiredService<IConfiguration>();
-                // Set password with the Secret Manager tool.
-                // dotnet user-secrets set SeedUserPW <pw>
-
-                var testUserPw = config["SeedUserPW"];
-
-                try
-                {
-                    SeedData.Initialize(services, testUserPw).Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                    throw;
-                }
+                Config = services.GetRequiredService<IConfiguration>();
+                MigradeDatabase(services);
             }
 
             host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole();
+                    factory.AddDebug();
+                    factory.AddFilter("Console", level => level >= LogLevel.Information);
+                    factory.AddFilter("Debug", level => level >= LogLevel.Information);
+                })
+                .UseKestrel(options =>
+                {
+                    int port = Config.GetValue<int>("Ssl:Port");
+                    options.AddServerHeader = false;
+                    options.Listen(IPAddress.Loopback, port, listenOptions =>
+                    {
+                        // Configure SSL
+                        X509Certificate2 serverCert = LoadCertificate();
+                        listenOptions.UseHttps(serverCert);
+                    });
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+        }
+
+        private static void MigradeDatabase(IServiceProvider services)
+        {
+            var dbContext = services.GetRequiredService<ApplicationDbContext>();
+
+            // Initiate database script migration
+            dbContext.Database.Migrate();
+
+            // Set password with the Secret Manager tool.
+            // dotnet user-secrets set SeedUserPW <pw>
+            var testUserPw = Config["Database:SeedUserPW"];
+
+            try
+            {
+                SeedData.Initialize(services, testUserPw).Wait();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while seeding the database.");
+                throw;
+            }
+        }
+
+        private static X509Certificate2 LoadCertificate()
+        {
+            Assembly assembly = typeof(Startup).GetTypeInfo().Assembly;
+            EmbeddedFileProvider embeddedFileProvider = new EmbeddedFileProvider(assembly, "final");
+            IFileInfo certificateFileInfo = embeddedFileProvider.GetFileInfo(Config["Ssl:Certificate:FileName"]);
+
+            using (Stream certificateStream = certificateFileInfo.CreateReadStream())
+            {
+                byte[] certificatePayload;
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    certificateStream.CopyTo(memoryStream);
+                    certificatePayload = memoryStream.ToArray();
+                }
+
+                return new X509Certificate2(certificatePayload, Config["Ssl:Certificate:Password"]);
+            }
+
+            //string certFileName = Config["Ssl:Certificate:FileName"];
+            //string certPassword = Config["Ssl:Certificate:Password"];
+
+            //return new X509Certificate2(certFileName, certPassword);
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,8 +5,8 @@ using ContactManager.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -18,14 +18,15 @@ namespace ContactManager
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        private IHostingEnvironment Environment { get; }
+
         public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
             Environment = env;
         }
-
-        public IConfiguration Configuration { get; }
-        private IHostingEnvironment Environment { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -36,10 +37,23 @@ namespace ContactManager
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            var skipHTTPS = Configuration.GetValue<bool>("LocalTest:skipHTTPS");
-            // requires using Microsoft.AspNetCore.Mvc;
-            services.Configure<MvcOptions>(options =>
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.AddSingleton<ISmsSender, AuthMessageSender>();
+
+            bool skipHTTPS = Configuration.GetValue<bool>("Ssl:Skip");
+
+            // requires:
+            // using Microsoft.AspNetCore.Authorization;
+            // using Microsoft.AspNetCore.Mvc.Authorization;
+            // using Microsoft.AspNetCore.Mvc;
+            services.AddMvc(options =>
             {
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+                options.SslPort = Configuration.GetValue<int>("Ssl:Port");
+
                 // Set LocalTest:skipHTTPS to true to skip SSL requrement in 
                 // debug mode. This is useful when not using Visual Studio.
                 if (Environment.IsDevelopment() && !skipHTTPS)
@@ -48,35 +62,38 @@ namespace ContactManager
                 }
             });
 
-            services.AddMvc();
             //.AddRazorPagesOptions(options =>
             //{
             //    options.Conventions.AuthorizeFolder("/Account/Manage");
             //    options.Conventions.AuthorizePage("/Account/Logout");
             //});
 
-            services.AddSingleton<IEmailSender, EmailSender>();
-            services.AddSingleton<ISmsSender, AuthMessageSender>();
-
-            // requires: using Microsoft.AspNetCore.Authorization;
-            //           using Microsoft.AspNetCore.Mvc.Authorization;
-            services.AddMvc(config =>
+            services.AddHsts(options =>
             {
-                var policy = new AuthorizationPolicyBuilder()
-                                 .RequireAuthenticatedUser()
-                                 .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
+                options.Preload = Configuration.GetValue<bool>("HstsOptions:Preload");
+                options.IncludeSubDomains = Configuration.GetValue<bool>("HstsOptions:IncludeSubDomains");
+                options.MaxAge = TimeSpan.FromDays(Configuration.GetValue<double>("HstsOptions:MaxAge"));
+                options.ExcludedHosts.Add(Configuration["HstsOptions:ExcludedHosts"]);
+            });
+
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status301MovedPermanently;
+                options.HttpsPort = Configuration.GetValue<int>("Ssl:Port");
+            });
+
+            services.AddAntiforgery(options =>
+            {
+                options.Cookie.Name = "_af";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.HeaderName = "X-XSRF-TOKEN";
             });
 
             // Authorization handlers.
-            services.AddScoped<IAuthorizationHandler,
-                                  ContactIsOwnerAuthorizationHandler>();
-
-            services.AddSingleton<IAuthorizationHandler,
-                                  ContactAdministratorsAuthorizationHandler>();
-
-            services.AddSingleton<IAuthorizationHandler,
-                                  ContactManagerAuthorizationHandler>();
+            services.AddScoped<IAuthorizationHandler, ContactIsOwnerAuthorizationHandler>();
+            services.AddSingleton<IAuthorizationHandler, ContactAdministratorsAuthorizationHandler>();
+            services.AddSingleton<IAuthorizationHandler, ContactManagerAuthorizationHandler>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -90,10 +107,11 @@ namespace ContactManager
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseAuthentication();
 
             app.UseMvcWithDefaultRoute();
